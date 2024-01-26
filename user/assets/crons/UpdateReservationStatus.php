d9ce80b5ac0a727f3b0f2e421a5a422873470d19
dan0207 <145758063+dan0207@users.noreply.github.com>
2024-01-26 17:19:03 +0800

    qwe

--- user/assets/crons/UpdateReservationStatus.php
+++ user/assets/crons/UpdateReservationStatus.php
@@ -7,6 +7,6 @@ $sql = "UPDATE bookreserve
         SET status = 'to pickup'
         WHERE status = 'hold'
         AND timestamp <= NOW() - INTERVAL 1 MINUTE";
-
+echo "hello world\n";
 mysqli_query($db, $sql);
 mysqli_close($db);
