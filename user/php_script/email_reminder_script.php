45c2c4c29208abcb422a3675a91862d8fbe88b8a
dan0207 <145758063+dan0207@users.noreply.github.com>
2024-01-26 20:45:24 +0800

    qwe

--- user/php_script/email_reminder_script.php
+++ user/php_script/email_reminder_script.php
@@ -18,8 +18,10 @@ require('db_local_connection.php');
 
 $user_token = $_SESSION['user_token'];
 
-// $sql = "SELECT email FROM users WHERE user_token = '$user_token'";
-// $result = mysqli_query($db, $sql);
 
-// echo $result;
+$sql = "SELECT email FROM users WHERE user_token = '$user_token'";
+$result = mysqli_query($db, $sql);
+$email = $result->fetch_assoc();
+
+echo $email;
 echo 'Hello';
