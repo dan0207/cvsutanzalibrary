118066cdbb89cad5a9cd6223637727418a4baa50
dan0207 <145758063+dan0207@users.noreply.github.com>
2024-01-26 20:22:30 +0800

    qwe

--- user/php_script/cron_script.php
+++ user/php_script/cron_script.php
@@ -7,3 +7,13 @@ $sql = "CALL UpdateBookStatus();";
 mysqli_query($db, $sql);
 
 
+$currentDate = new DateTime();
+$formattedDate = $currentDate->format('Y-m-d H:i:s');
+
+$to = "danilojr.abancia@cvsu.edu.ph";
+$subject = "Your Email Subject";
+$message = "$formattedDate";
+$headers = "From: Campus Librarian";
+
+// Send email
+mail($to, $subject, $message, $headers);
