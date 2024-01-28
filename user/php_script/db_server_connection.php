1bd3bed47f5499c7476ce9f359caca25877ea41b
dan0207 <145758063+dan0207@users.noreply.github.com>
2024-01-28 21:56:21 +0800

    qwe

--- user/php_script/db_server_connection.php
+++ user/php_script/db_server_connection.php
@@ -1,9 +1,16 @@
 <?php
 
-$serverHost = '192.168.1.32'; 
-$serverUsername = 'user';
-$serverPassword = 'User8888$$$';
-$serverDatabase = 'cvsutanzalibrary-clone';
+
+// $serverHost = '192.168.1.32'; 
+// $serverUsername = 'user';
+// $serverPassword = 'User8888$$$';
+// $serverDatabase = 'cvsutanzalibrary-clone';
+
+$serverHost = 'localhost'; 
+$serverUsername = 'root';
+$serverPassword = '';
+$serverDatabase = 'cvsutanzalib_db';
+
 
 $db_server = new mysqli($serverHost, $serverUsername, $serverPassword, $serverDatabase);
 
