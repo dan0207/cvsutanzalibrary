2fd80a0342e674b13e76efd8725f2bd1b8d33159
Blitzen01 <ronjustin22@gmail.com>
2024-01-30 12:38:59 +0800

    Analytics Daily report

--- admin/admin/circulation.php
+++ admin/admin/circulation.php
@@ -13,24 +13,6 @@
 
         <link rel="stylesheet" href="../assets/style/style.css">
         
-        <style>
-            
-        /* admin transaction | remarks change of colors */
-        .text-danger {
-            color: red;
-        }
-
-        .text-success {
-            color: green;
-        }
-
-        .text-warning {
-            color: orange;
-        }
-        /* admin transaction | remarks change of colors */
-
-
-        </style>
     </head>
 
     <body>
