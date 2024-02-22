65d189deb12ba14ef0108cedda28e3ec51cda577
dan0207 <145758063+dan0207@users.noreply.github.com>
2024-02-22 11:33:25 +0800

    123

--- user/php_script/db_local_connection.php
+++ user/php_script/db_local_connection.php
@@ -1,14 +1,14 @@
 <?php
 
-// $servername = "154.41.240.153";
-// $username = "u303407946_cvsutanzalib";
-// $password = "uTQ^zBHA*k1";
-// $database = "u303407946_cvsutanzalib";
+$servername = "154.41.240.153";
+$username = "u303407946_cvsutanzalib";
+$password = "uTQ^zBHA*k1";
+$database = "u303407946_cvsutanzalib";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "cvsutanzalib_db";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "cvsutanzalib_db";
 
 $db = new mysqli($servername, $username, $password, $database);
 mysqli_query($db, "SET time_zone = '+08:00'");
